b8438e9e4d4ff501c8927004b7229c6f2cc6da1f
salim ameur <salim.ameur@gmail.com>
2020-10-21 10:39:13 +0200

    user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  fistName:String,
-  lastName:String,
+  fistName: String,
+  lastName: String,
+  role: { type: String, enum: ["teacher", "student"], default: "student" },
   email: { type: String, required: true },
   password: { type: String, required: true },
 });
