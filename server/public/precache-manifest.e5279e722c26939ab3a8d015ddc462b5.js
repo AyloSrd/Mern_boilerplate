8bc0b0b8894c7e652aa349de62c2b313e9c25320
AyloSrd <aylo.srd@gmail.com>
2020-10-22 11:58:01 +0200

    build number1

---
+++ server/public/precache-manifest.e5279e722c26939ab3a8d015ddc462b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ff4131a810136d0dc7a54d559ff1bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "556845596278cb75250e",
+    "url": "/static/css/2.ce280647.chunk.css"
+  },
+  {
+    "revision": "ee489347dd56e1f37981",
+    "url": "/static/css/main.622eadd6.chunk.css"
+  },
+  {
+    "revision": "556845596278cb75250e",
+    "url": "/static/js/2.604013c4.chunk.js"
+  },
+  {
+    "revision": "23e9e8d00746a8ffa05a4f283b141d14",
+    "url": "/static/js/2.604013c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee489347dd56e1f37981",
+    "url": "/static/js/main.aac6f382.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/static/media/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "861720fa94b80958075f620b2c6e8fb9",
+    "url": "/static/media/nucleo-icons.861720fa.woff"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/static/media/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/static/media/nucleo-icons.ff208b8d.woff2"
+  }
+]);
